3fb10994e7628688d82b352a6a1b2c7fa5237d57
GuillaumeMCK <89449753+GuillaumeMCK@users.noreply.github.com>
2022-06-12 03:04:36 +0200

    update hand controller + add catalogues page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'list-catalogs',
+    loadChildren: () => import('./list-catalogues/list-catalogues.module').then( m => m.ListCatalogsPageModule)
+  },
 ];
 
 @NgModule({
